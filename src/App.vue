983e09b1c2907109d17b7faac0e3b621742075d0
surazz501 <surazz501@gmail.com>
2020-02-21 23:10:12 +0545

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+   <AddTodo v-on:add-todo="addnewtodo" />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
+import axios from 'axios';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos,AddTodo
+  },
+  methods:{
+deleteTodo(id){
+   axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  .then(() => {this.todos = this.todos.filter(todo => todo.id !== id)})
+  .catch(err => console.log(err));
+    
+},
+addnewtodo(newTodo){
+  const {title,completed} = newTodo;
+
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title,completed
+  })
+  .then(res => this.todos = [...this.todos,res.data])
+  .catch(err => console.log(err));
+
+  //[...this.todos,res.data] allow to add item res.data to array this.todos
+
+}
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res=> this.todos=res.data)
+    .catch(err => console.log(err))
+  },
+  data(){
+    return {
+      todos: []
   }
+}
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+*{
+  box-sizing: border-box;
+  margin:0px;
+  padding:0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.5;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+        <input type='text' v-model="title" name='title' >
+        <button type="submit">Submit</button>
+       </form> </div>
+</template>
+<script>
+export default {
+    name:"AddTodo",
+    methods:{
+addTodo(){
+    event.preventDefault();
+    const newTodo={
+            title:this.title,
+        completed:false
+    }
+    //send upto  parent
+    this.$emit("add-todo",newTodo);
+   this.title='';
+    }
+    },
+    data(){
+        return{
+            title:''
+        }
+    }
+}
+</script>
+<style scoped>
+ form {
+    display: flex;
+  }
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+  input[type="submit"] {
+    flex: 2;
+    color: green;
+
+  }
+
+</style>
